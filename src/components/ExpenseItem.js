b93c11de23cfaeb955a5a6df4c03c7ac01ec0a02
Sarthak Shukla <sarthakshukla72@gmail.com>
2022-09-18 21:11:01 +0530

    setup folder structure and added expenseItem compo

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function ExpenseItem() {
+  return (
+    <div>
+        <div>
+            <div>March 29 2023</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$299</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ExpenseItem
