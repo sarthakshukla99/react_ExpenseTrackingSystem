d578d251516e39b178cc6d96cd8272bdcefdd20e
Sarthak Shukla <sarthakshukla72@gmail.com>
2022-09-20 22:19:03 +0530

    added newExpense for adding new expense items

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
 
@@ -26,6 +27,7 @@ const App = () => {
 
     return(
         <div className="App">
+            <NewExpense/>
             <Expenses expenses={expenses}/>
         </div>
     );
